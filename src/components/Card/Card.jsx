fccf7e76040ada7b06cfa020e6ff209734099348
Anrii <andrejsalogub3@gmail.com>
2022-11-06 12:04:28 +0200

    add flipping card functionality and animation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Card.css'
 
-export const Card = ({ card, handleChoice }) => {
+export const Card = ({ card, handleChoice, flipped }) => {
     const handleClick = () => {
         handleChoice(card)
     }
 
     return (
         <div className="card">
-            <div>
+            <div className={flipped ? 'flipped' : ''}>
                 <img className="front" src={card.src} alt="card front" />
                 <img
                     onClick={() => handleClick()}
